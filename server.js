6952c90a9a261360ba3e7486f24e0014c888c588
Nate Suver <nate.suver@gmail.com>
2017-10-29 13:17:01 -0400

    add new behaviors

--- server.js
+++ server.js
@@ -4,19 +4,25 @@ http    = require('http').createServer(app),
 io      = require('socket.io')(http);
 
 http.listen(3000);
-
+var gameState={score: {'a':0,'b':0 },players:[], started:false};
 io.on('connection', function (socket) {
-    var gameState={score: {'a':0,'b':0 }};
+    
    // console.log("Connection to Game " + socket.handshake.query['gameId'] + " established!!!");
-    socket.emit('handshake', {status : 'welcome'});
-    //socket.on('stateChanged', function (data) {
-   //   socket.broadcast.emit('stateChange', gameState);
-   // });
+    socket.emit('handshake', JSON.stringify(gameState));
+    
+    socket.on('stateChange', function (playerData) {
+      gameState.players = JSON.parse(playerData);
+      socket.broadcast.emit('stateChange', JSON.stringify(gameState));
+    });
    
     socket.on('impactDetected', function (impactData) {
       socket.broadcast.emit('impact', JSON.stringify(impactData));
     });
 
+    socket.on('gameStarted', function (impactData) {
+      gameState.started = true;
+      io.sockets.emit('stateChange', JSON.stringify(gameState));
+    });
    //Arguments:  paddleData is a json encoded string e,g. {l:999,p:999}.  l is the location of the paddle on the board, p is the player id.  Both are integers.
     socket.on('paddleChange', function (paddleData) {
       socket.broadcast.emit('paddleChange', JSON.stringify(paddleData));
@@ -25,11 +31,16 @@ io.on('connection', function (socket) {
     socket.on('score', function(team) {
       gameState.score[team]++;
       if (gameState.score[team] >=15) {
+        gameState.started = false;
+        io.sockets.emit('stateChange', JSON.stringify(gameState));
         io.sockets.emit('gameOver', JSON.stringify(gameState.score));
       } else {
         io.sockets.emit('scoreChange', JSON.stringify(gameState.score));
       }
     });    
+    socket.on('disconnect', function () {
+      io.emit('disconnected');
+    });
 });
 
 /*
