233af8503fd41c69ef4913724d96668b644e3197
Nate Suver <nate.suver@gmail.com>
2017-11-10 23:45:34 -0500

    record number of paddle strikes

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ http    = require('http').createServer(app),
 io      = require('socket.io')(http);
 
 http.listen(3000);
-var gameState, scoreUpperBound = 15;
+scoreUpperBound = 600;
 function initGameState() {
-  gameState={score: {'a':0,'b':0 },players:[], started:false, gameId: 0};
+  return {score: {'a':0,'b':0 },players:{}, started:false, gameId: 0};
 }
-initGameState();
 io.on('connection', function (socket) {
-    gameState.gameId = socket.handshake.query['gameId'];
+    this.gameState = initGameState();
+    var self = this;
+    this.gameState.gameId = socket.handshake.query['gameId'];
    // console.log("Connection to Game " + socket.handshake.query['gameId'] + " established!!!");
-    socket.emit('handshake', JSON.stringify(gameState));
+    socket.emit('handshake', JSON.stringify(this.gameState));
     
     socket.on('stateChange', function (playerData) {
-      gameState.players = JSON.parse(playerData);
-      socket.broadcast.emit('stateChange', JSON.stringify(gameState));
+      socket.broadcast.emit('stateChange', JSON.stringify(self.gameState));
     });
    
     socket.on('impactDetected', function (impactData) {
@@ -24,8 +24,15 @@ io.on('connection', function (socket) {
     });
 
     socket.on('gameStarted', function (data) {
-      gameState.started = true;
-      io.sockets.emit('stateChange', JSON.stringify(gameState));
+      self.gameState.started = true;
+      io.sockets.emit('stateChange', JSON.stringify(self.gameState));
+    });
+    socket.on('hit', function (playerId) {
+      var state = self.gameState;
+      if (!state.players["p"+playerId]) {
+        state.players["p"+playerId]={id: playerId, hits:0};
+      }
+      state.players["p"+playerId].hits++;
     });
    //Arguments:  paddleData is a json encoded string e,g. {l:999,p:999}.  l is the location of the paddle on the board, p is the player id.  Both are integers.
     socket.on('paddleChange', function (paddleData) {
@@ -33,13 +40,13 @@ io.on('connection', function (socket) {
     });
     
     socket.on('score', function(team) {
-      gameState.score[team]++;
-      if (gameState.score[team] >=scoreUpperBound) {
+      self.gameState.score[team]++;
+      if (self.gameState.score[team] >=scoreUpperBound) {
         initGameState();
-        io.sockets.emit('stateChange', JSON.stringify(gameState));
-        io.sockets.emit('gameOver', JSON.stringify(gameState.score));
+        io.sockets.emit('stateChange', JSON.stringify(self.gameState));
+        io.sockets.emit('gameOver', JSON.stringify(self.gameState));
       } else {
-        io.sockets.emit('scoreChange', JSON.stringify(gameState.score));
+        io.sockets.emit('scoreChange', JSON.stringify(self.gameState.score));
       }
     });    
     socket.on('disconnect', function () {
